c30ed0cfa19fcdfc086305925d1d9cfe740987fd
Beatriz-Claus <61596748+Beatriz-Claus@users.noreply.github.com>
2020-06-17 15:02:57 -0300

    Dojo 2 - Finalizado

---
+++ IngressoVip.cs
@@ -0,0 +1,15 @@
+using System;
+namespace Aula19Dojo2
+{
+    public class IngressoVip : Ingresso
+    {
+        public float valorAdicional = 50.0f;
+
+        public void AdicionarValor(){
+            float newValor = IngressoPreco + valorAdicional;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("O valor VIP é : {0}",newValor);
+            Console.ResetColor();
+        }
+    }
+}
